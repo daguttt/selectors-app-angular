c5784bb75f1c1b9fa939cb3b80818b1c63a289af
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-09-09 12:07:52 -0500

    Class 285: Initialize FormGroup

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SelectorPageComponent } from './selector-page/selector-page.component';
 import { CountriesRoutingModule } from './countries-routing.module';
 
 @NgModule({
-  imports: [CountriesRoutingModule],
+  imports: [CommonModule, CountriesRoutingModule, ReactiveFormsModule],
   declarations: [SelectorPageComponent],
   exports: [SelectorPageComponent],
 })
--- src/app/countries/selector-page/selector-page.component.ts
+++ src/app/countries/selector-page/selector-page.component.ts
@@ -1,7 +1,17 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
 })
-export class SelectorPageComponent {}
+export class SelectorPageComponent {
+  countriesForm: FormGroup = this.fb.group({
+    continent: ['', Validators.required],
+  });
+  constructor(private fb: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log(this.countriesForm.value);
+  }
+}
